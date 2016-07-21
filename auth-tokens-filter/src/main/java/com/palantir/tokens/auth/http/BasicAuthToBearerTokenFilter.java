27bd92dd74bfb0b4ae806737c7975d617a1fa625
ags799 <ags799@users.noreply.github.com>
2016-07-21 06:21:32 -0400

    Add BasicAuthToBearerTokenFilter (#14)
    When applied converts Basic authentication header to a Bearer authentication header with the password bytes treated as the token.

---
+++ auth-tokens-filter/src/main/java/com/palantir/tokens/auth/http/BasicAuthToBearerTokenFilter.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tokens.auth.http;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+import com.google.common.io.BaseEncoding;
+import com.google.common.net.HttpHeaders;
+import com.palantir.tokens.auth.AuthHeader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Objects;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * A {@link Filter} that replaces basic auth with a bearer token.
+ * <p>
+ * It assumes that the bearer token is held in the password field of the basic auth credentials. It assumes that the
+ * bearer token is base-64 encoded.
+ */
+public class BasicAuthToBearerTokenFilter implements Filter {
+    private static final Logger log = LoggerFactory.getLogger(BasicAuthToBearerTokenFilter.class);
+
+    private static final String BASIC_AUTH_STR = "Basic";
+    private static final BaseEncoding BASE_64_ENCODING = BaseEncoding.base64Url();
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public final void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        ServletRequest updatedRequest = addBearerTokenIfBasicAuth(request);
+        chain.doFilter(updatedRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+
+    private ServletRequest addBearerTokenIfBasicAuth(ServletRequest request) {
+        if (request instanceof HttpServletRequest) {
+            HttpServletRequest httpRequest = (HttpServletRequest) request;
+            String rawAuthHeader = httpRequest.getHeader(HttpHeaders.AUTHORIZATION);
+            return addBearerTokenIfBasicAuth(httpRequest, rawAuthHeader);
+        } else {
+            log.warn("Request is not an HttpServletRequest.");
+            return request;
+        }
+    }
+
+    private ServletRequest addBearerTokenIfBasicAuth(HttpServletRequest request, String rawAuthHeader) {
+        if (isBasicAuth(rawAuthHeader)) {
+            AuthHeader authHeader;
+            try {
+                authHeader = base64DecodePassword(rawAuthHeader);
+            } catch (IllegalArgumentException e) {
+                log.warn("Could not decode password in basic auth header", e);
+                return request;
+            }
+            return addBearerToken(request, authHeader);
+        } else {
+            log.warn("Auth header is not basic auth.");
+            return request;
+        }
+    }
+
+    private ServletRequest addBearerToken(HttpServletRequest request, final AuthHeader authHeader) {
+        return new HttpServletRequestWrapper(request) {
+            @Override
+            public String getHeader(String name) {
+                if (Objects.equals(name, HttpHeaders.AUTHORIZATION)) {
+                    return authHeader.toString();
+                } else {
+                    return super.getHeader(name);
+                }
+            }
+        };
+    }
+
+    private AuthHeader base64DecodePassword(String rawAuthHeader) {
+        String base64Credentials = rawAuthHeader.substring(BASIC_AUTH_STR.length()).trim();
+        String credentials;
+        try {
+            credentials = new String(BASE_64_ENCODING.decode(base64Credentials), StandardCharsets.UTF_8);
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("Could not decode credentials from auth header: " + e.getMessage());
+        }
+        checkArgument(credentials.contains(":"), "Credentials lack colon character (:).");
+        String password = credentials.split(":", 2)[1];
+        return AuthHeader.valueOf(password);
+    }
+
+    private boolean isBasicAuth(String rawAuthHeader) {
+        return rawAuthHeader != null && rawAuthHeader.contains(BASIC_AUTH_STR);
+    }
+
+}
---
+++ auth-tokens-filter/src/test/java/com/palantir/tokens/auth/http/BasicAuthToBearerTokenFilterTest.java
@@ -0,0 +1,154 @@
+/*
+ * Copyright 2016 Palantir Technologies, Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.palantir.tokens.auth.http;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.common.io.BaseEncoding;
+import com.google.common.net.HttpHeaders;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletRequestWrapper;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import org.hamcrest.MatcherAssert;
+import org.hamcrest.core.Is;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class BasicAuthToBearerTokenFilterTest {
+
+    private static final BaseEncoding BASE_64_ENCODING = BaseEncoding.base64Url();
+
+    @Mock
+    private HttpServletRequest httpServletRequest;
+    @Mock
+    private FilterChain chain;
+    @Captor
+    private ArgumentCaptor<HttpServletRequest> requestArgumentCaptor;
+
+    private BasicAuthToBearerTokenFilter tokenFilter;
+
+    @Before
+    public final void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+        tokenFilter = new BasicAuthToBearerTokenFilter();
+    }
+
+    @Test
+    public final void testExpectedHeader() throws Exception {
+        String password = "password";
+        setPassword(password);
+        filter();
+        assertChainRequestHasAuthHeader("Bearer " + password);
+    }
+
+    @Test
+    public final void testNotInstanceOfHttpServletRequest() throws Exception {
+        // servletRequestWrapper is not instanceof HttpServletRequest
+        ServletRequestWrapper servletRequestWrapper = new ServletRequestWrapper(httpServletRequest);
+        filter(servletRequestWrapper);
+        assertFilteredRequestIs(servletRequestWrapper);  // filtered request is unchanged
+    }
+
+    @Test
+    public final void testNullAuthHeader() throws Exception {
+        setAuthHeader(null);
+        filter();
+        assertRequestUnchanged();
+    }
+
+    @Test
+    public final void testAuthHeaderNotBasicAuth() throws IOException, ServletException {
+        setAuthHeader("something unexpected");
+        filter();
+        assertRequestUnchanged();
+    }
+
+    @Test
+    public final void testCannotDecode() throws IOException, ServletException {
+        String encodedCreds = "not base-64 encoded";
+        setBasicAuthHeader(encodedCreds);
+        filter();
+        assertRequestUnchanged();
+    }
+
+    @Test
+    public final void testLacksColon() throws IOException, ServletException {
+        String credentials = "lacks-colon";
+        setCredentials(credentials);
+        filter();
+        assertRequestUnchanged();
+    }
+
+    private void filter() throws IOException, ServletException {
+        filter(httpServletRequest);
+    }
+
+    private void filter(ServletRequest request) throws IOException, ServletException {
+        tokenFilter.doFilter(request, null, chain);
+    }
+
+    private void setPassword(String password) {
+        String credentials = "foo:" + password;
+        setCredentials(credentials);
+    }
+
+    private void setCredentials(String credentials) {
+        String encodedCreds = BASE_64_ENCODING.encode(credentials.getBytes(StandardCharsets.UTF_8));
+        setBasicAuthHeader(encodedCreds);
+    }
+
+    private void setBasicAuthHeader(String encodedCreds) {
+        String authHeader = "Basic " + encodedCreds;
+        setAuthHeader(authHeader);
+    }
+
+    private void setAuthHeader(String authHeader) {
+        when(httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION)).thenReturn(authHeader);
+    }
+
+    private void assertChainRequestHasAuthHeader(String expectedAuthHeader) throws IOException, ServletException {
+        HttpServletRequest value = (HttpServletRequest) getChainRequest();
+        String actualAuthHeader = value.getHeader(HttpHeaders.AUTHORIZATION);
+        MatcherAssert.assertThat(actualAuthHeader, Is.is(expectedAuthHeader));
+    }
+
+    private void assertRequestUnchanged() throws IOException, ServletException {
+        assertFilteredRequestIs(httpServletRequest);
+    }
+
+    private void assertFilteredRequestIs(ServletRequest request) throws IOException, ServletException {
+        ServletRequest chainRequest = getChainRequest();
+        MatcherAssert.assertThat(chainRequest, Is.is(request));
+    }
+
+    private ServletRequest getChainRequest() throws IOException, ServletException {
+        verify(chain).doFilter(requestArgumentCaptor.capture(), Mockito.<ServletResponse>any());
+        return requestArgumentCaptor.getValue();
+    }
+
+}
