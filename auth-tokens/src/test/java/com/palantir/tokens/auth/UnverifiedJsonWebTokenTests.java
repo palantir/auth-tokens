b2f32395ba21796ae8f0939128be42bbdb9c72a5
Dan Sanduleac <dsanduleac@palantir.com>
2019-07-15 16:20:09 +0100

    imports...

--- auth-tokens/src/test/java/com/palantir/tokens/auth/UnverifiedJsonWebTokenTests.java
+++ auth-tokens/src/test/java/com/palantir/tokens/auth/UnverifiedJsonWebTokenTests.java
@@ -17,7 +17,6 @@
 package com.palantir.tokens.auth;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
 
 import com.palantir.logsafe.SafeArg;
 import com.palantir.logsafe.testing.Assertions;
