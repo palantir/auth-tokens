fde1de382c4552f8a5d850ba1853b5fd95ea600c
Heok Hong Low <heok.hong.low@gmail.com>
2026-03-12 12:45:07 +0800

    CR

--- auth-tokens/src/main/java/com/palantir/tokens/auth/BearerToken.java
+++ auth-tokens/src/main/java/com/palantir/tokens/auth/BearerToken.java
@@ -19,16 +19,99 @@ package com.palantir.tokens.auth;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.palantir.logsafe.DoNotLog;
+import com.palantir.logsafe.Preconditions;
+import com.palantir.logsafe.exceptions.SafeIllegalArgumentException;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.util.BitSet;
 
 /** Value class representing an authentication bearer token. */
 @DoNotLog
-public abstract class BearerToken {
-    @JsonValue
-    @DoNotLog
-    public abstract String getToken();
+public final class BearerToken {
+
+    private static final BitSet ALLOWED_CHARACTERS = createAllowedCharacters();
+
+    private static BitSet createAllowedCharacters() {
+        BitSet allowedCharacters = new BitSet();
+        allowedCharacters.set('A', 'Z' + 1);
+        allowedCharacters.set('a', 'z' + 1);
+        allowedCharacters.set('0', '9' + 1);
+        allowedCharacters.set('-');
+        allowedCharacters.set('.');
+        allowedCharacters.set('_');
+        allowedCharacters.set('~');
+        allowedCharacters.set('+');
+        allowedCharacters.set('/');
+        return allowedCharacters;
+    }
+
+    private final String token;
+    private final byte[] tokenBytes;
+
+    private BearerToken(String token) {
+        checkValidBearerToken(token);
+        this.token = token;
+        this.tokenBytes = getTokenAsBytes(token);
+    }
 
     @JsonCreator
     public static BearerToken valueOf(String token) {
-        return ImmutableBearerToken.valueOf(token);
+        return new BearerToken(token);
+    }
+
+    @JsonValue
+    @DoNotLog
+    public String getToken() {
+        return token;
+    }
+
+    static byte[] getTokenAsBytes(String token) {
+        return token.getBytes(StandardCharsets.US_ASCII);
+    }
+
+    // Optimized validity check instead of using regular expressions
+    private static void checkValidBearerToken(String token) {
+        Preconditions.checkArgument(token != null, "BearerToken cannot be null");
+        Preconditions.checkArgument(!token.isEmpty(), "BearerToken cannot be empty");
+        int length = token.length();
+        int cursor = 0;
+
+        for (; cursor < length; cursor++) {
+            if (!ALLOWED_CHARACTERS.get(token.charAt(cursor))) {
+                break;
+            }
+        }
+
+        // Need at least one valid character
+        if (cursor == 0) {
+            throw new SafeIllegalArgumentException("Invalid BearerToken");
+        }
+
+        // Only trailing '=' is allowed after valid characters
+        for (; cursor < length; cursor++) {
+            if (token.charAt(cursor) != '=') {
+                throw new SafeIllegalArgumentException("Invalid BearerToken");
+            }
+        }
+    }
+
+    @Override
+    @DoNotLog
+    public String toString() {
+        return token;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other instanceof BearerToken bearerToken) {
+            return MessageDigest.isEqual(bearerToken.tokenBytes, tokenBytes);
+        }
+        return false;
+    }
+
+    @Override
+    @DoNotLog
+    public int hashCode() {
+        return token.hashCode();
     }
 }
